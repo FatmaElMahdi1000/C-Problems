b3db4d1c0e28bc4b7e68ca83b0240c5caa812a87
FatmaElMahdi1000 <f69246984@gmail.com>
2025-12-07 11:33:10 +0200

    solving problems!

--- Code_Wars_Problems/Sentence_smash.c
+++ Code_Wars_Problems/Sentence_smash.c
@@ -1,34 +1,36 @@
-#include "stdlib.h"
-#include "string.h"
-#include "stdio.h"
+#include"stdio.h"
+#include"stdlib.h"
+#include"string.h"
 
- 
-char *smash(const char *const words[], size_t count)
+
+char *smash_together(char *arr[], int arr_size)
 {
-    // calculate total length
-    size_t total_len = 1; // for null terminator
-    for (size_t i = 0; i < count; i++)
-        total_len += strlen(words[i]);
-
-    char *result = (char*) calloc(total_len, sizeof(char));
-    if (!result) return NULL;
-
-    // copy words into result
-    for (size_t i = 0; i < count; i++)
-        strcat(result, words[i]);
-
-    return result;  
-}
-
-int main() {
-    const char *arr[] = {"hello", "there", "I am", "Fatma"};
-    size_t count = sizeof(arr) / sizeof(arr[0]);
-
-    char *r = smash(arr, count);
-    if (r) {
-        printf("%s\n", r);
-        free(r);
+    int char_count = 0; //to exclude the newline;
+    for(int i =0; i <arr_size; i++)
+    {
+        char_count = char_count + strlen(arr[i]);
+    }
+    char *arr2 = (char*) calloc(char_count+1+arr_size,sizeof(char)); // added one for the '\0' of the entire string and 4(which the size of the array for fours spaces)
+    for(int j =0; j < arr_size; j++)
+    {
+        strcat(arr2, arr[j]);
+        if(j != arr_size - 1)
+        {
+            strcat(arr2, " ");
+        }
     }
 
-    return 0;
+    return arr2;
+
+}
+
+int main()
+{
+    char *arr[] = {"Hi", "there", "this is", "Fatma"};
+    int arr_size = sizeof(arr)/sizeof(arr[0]); //sizeof the entire array / the size of one element.
+    printf("Size of the array: %zu\n", arr_size);
+    char *r = smash_together(arr, arr_size);
+    printf("%s\n", r);
+    free(r);
+    r = NULL;
 }
