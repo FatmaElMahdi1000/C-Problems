070f43b9eecd9d92b8db9317647dddf50b8d9bd5
FatmaElMahdi1000 <f69246984@gmail.com>
2025-12-18 17:46:38 +0200

    solving problems

--- W3r_Problems/six_sided_die.c
+++ W3r_Problems/six_sided_die.c
@@ -5,14 +5,25 @@
 
 int roll(int *p_num)
 {
+
     srand(time(NULL)); //seeds srand so each run is different 
-    int result = (rand() % *p_num) + 1;   //rand() % num, generate number from 0 to num-1, looks like range
-    return result;
+    int counter = 0; //for creating new line
+    for(int i = 0; i <20; i++)
+    {
+        int result = (rand() % *p_num) + 1;   //rand() % num, generate number from 0 to num-1, looks like range
+        printf("%d ", result);
+        counter++; //counter = counter + 1
+        if (counter % 5 == 0) //if divisible by 5 create a new line 
+        {
+            printf("\n");
+        }
+    }
+    return 0; //success
 }
 
 int main()
 {
     int num= 6;
     int *p_num = &num;
-    printf("the die output: %d\n", roll(p_num));
+    int r = roll(p_num);
 }
