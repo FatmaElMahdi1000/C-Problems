cdcf132772c2a4a6a80440c275200c2fc3fc09ec
FatmaElMahdi1000 <f69246984@gmail.com>
2026-02-28 16:20:08 +0200

    Coding.in.C!

--- Coding_in_C/Structs/TimeStructureCal.c
+++ Coding_in_C/Structs/TimeStructureCal.c
@@ -11,12 +11,12 @@ typedef struct
 
 //convert time_vals to seconds first, substracting then, convert the new time back to hrs. min, sec.
 
-int time_diff_Sec(time_val time1, time_val time2)
+int time_in_Sec(time_val time1, time_val time2)
 {
-    int hr_conv = 3600;
-    int min_conv = 60;
-    int diff = (time1.hr)*hr_conv+(time1.min)*min_conv+(time1.sec) - ((time2.hr)*hr_conv +(time2.min)*min_conv+(time2.sec)); 
-    return diff;
+    double hr_conv = 3600;
+    double min_conv = 60;
+    double TotalSec = (time1.hr)*hr_conv+(time1.min)*min_conv+(time1.sec) + ((time2.hr)*hr_conv +(time2.min)*min_conv+(time2.sec)); 
+    return TotalSec;
 }
 
 
@@ -24,16 +24,21 @@ int main()
 {
     time_val time1;
     time_val time2;
-    int hr_conv = 3600;
-    int min_conv = 60;
+    double hr_conv = 3600;
+    double min_conv = 60;
     printf("Enter the hrs mins secs respectively of the 1st time: ");
 
     scanf("%d%d%d", &time1.hr, &time1.min, &time1.sec);
     printf("Enter the hrs mins secs respectively of the 2nd time: ");
     scanf("%d%d%d", &time2.hr , &time2.min, &time2.sec);
-    int time_diff = time_diff_Sec(time1, time2);
-    int hr_diff  = time_diff % 60;
-    //code left 
     
+    int TotalSec = time_in_Sec(time1, time2);
+    //  % does not work with double.  
+    int hrs_num  =  TotalSec / 3600;
+    int remaining_sec =  TotalSec % 3600;
+    int min_num = remaining_sec / 60;
+    int sec_num = remaining_sec % 60;
 
+    printf("Calculated time after addition = %d Hrs: %d Mins : %d Secs\n", hrs_num, min_num, sec_num);
+     
 }
