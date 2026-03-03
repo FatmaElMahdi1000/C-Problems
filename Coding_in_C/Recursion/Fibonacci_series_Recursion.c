d00864d483164fb0f38ebf0acf0f9e7a7328d65a
FatmaElMahdi1000 <f69246984@gmail.com>
2026-03-03 23:53:23 +0200

    Coding.in.C!

--- Coding_in_C/Recursion/Fibonacci_series_Recursion.c
+++ Coding_in_C/Recursion/Fibonacci_series_Recursion.c
@@ -27,3 +27,18 @@ int main()
     fib(a,b, z, 1);
 
 }
+///DOCTOR'S way:
+// #include <stdio.h>
+
+// int fib(int n)
+// {
+//     if (n == 0 || n == 1)
+//         return n;
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// int main()
+// {
+//     printf("fib(10) is %d", fib(10));
+// }
