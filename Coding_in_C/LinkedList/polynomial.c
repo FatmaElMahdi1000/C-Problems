e13d9d58d8ae42880ba4c293f2de54a5811a847a
FatmaElMahdi1000 <f69246984@gmail.com>
2026-03-20 21:06:28 +0200

    Coding.in.C!

--- Coding_in_C/LinkedList/polynomial.c
+++ Coding_in_C/LinkedList/polynomial.c
@@ -16,7 +16,8 @@ node* getnode()
     //node memory allocation
     node* newnode = (node*) malloc(sizeof(node));
     printf("Enter the node's coefficient: ");
-    scanf("%lf", &newnode->coef);
+    //REMEMBER: %f for float %lf for double in scanf func.
+    scanf("%f", &newnode->coef);
     fflush(stdin);
     printf("Enter the node's Exponent: ");
     scanf("%d", &newnode->expo);
@@ -26,11 +27,50 @@ node* getnode()
 
 }
 
-
 //create the node by asking the user to enter the coefficient and the exponent of each node
 //How many number of nodes
+node* createnode(node* start, int n)
+{
+    node* newnode;
+    node* temp; //for traversal.
+    for(int i= 0; i <n; i++)
+    {
+        newnode = getnode();
+        if(start == NULL)
+        {
+            start = newnode;
+        }
+        else
+        {
+            temp = start;
+            while(temp->next != NULL)
+            {
+                temp=temp->next;
+            }
+            temp->next = newnode;
+
+        }
+
+    }
+
+    //returning start to main so, it knows from where to begin 
+    return start;
+}
+
+//Polynomial Display Func
+void display(node* start)
+{
+    node* temp = start;
+    while(temp != NULL)
+    {
+        printf("+ %2.f", temp->coef);
+        printf("X^ %d", temp->expo);
+        temp = temp->next;
+    }
 
 
+}
+//REMAINING PART: CALLING Func in main
 int main()
 {
     node* start = NULL;
